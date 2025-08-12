7353e5832bf09f29e07629f134857b5473621b70
nareshsmtai <nareshsmt.ai@outlook.com>
2025-08-12 13:05:32 +0530

    home page modified

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-home',
+  standalone:true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
 
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    particlesJS('particles-js', {
+      particles: {
+        number: { value: 80 },
+        size: { value: 3 },
+        move: { speed: 2 },
+        line_linked: {
+          enable: true,
+          color: '#00f0ff'
+        },
+        color: { value: '#00f0ff' }
+      }
+    });
+  }
+
+  login() {
+ this.router.navigate(['/login']);  }
+
+  loginAsGuest() {
+    console.log('Login as Guest clicked');
+  }
 }
